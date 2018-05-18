a7a56932d0c24764aa23cd7fff45f9594be8b00a
a1043309 <yoyowen1114@gmail.com>
2018-05-18 10:20:15 +0800

    Add files via upload

---
+++ all.js
@@ -0,0 +1,31 @@
+$(function(){
+  
+  $('.title_2').on('click', function(){
+    $('.qa').slideToggle();
+  });
+
+});
+
+function reset() {
+    var subject = document.getElementById('subject');
+    var con = document.getElementById('con');
+
+    if (!subject.value || subject.value != subject.defaultValue && confirm('確定要送出這則訊息嗎？ 審核及回覆時間約7-14天。')) {
+        subject.value = subject.defaultValue;
+    }
+    if (!con.value || con.value != con.defaultValue && confirm('確定要送出這則訊息嗎？ 審核及回覆時間約7-14天。')) {
+        con.value = con.defaultValue;
+    }
+}
+
+function reset2() {
+    var subject = document.getElementById('subject');
+    var con = document.getElementById('con');
+
+    if (!subject.value || subject.value != subject.defaultValue && confirm('Are you sure you want to send this message? The review and response time is about 7-14 days.')) {
+        subject.value = subject.defaultValue;
+    }
+    if (!con.value || con.value != con.defaultValue && confirm('Are you sure you want to send this message? The review and response time is about 7-14 days.')) {
+        con.value = con.defaultValue;
+    }
+}
